8f1179c785b1e4e52a5dd8deb4e7ce88b24f5f43
themegat <tshepomotsoeneng0@gmail.com>
2024-02-29 14:44:41 +0200

    Update motor speed input

---
+++ src/services/ConfigContext.tsx
@@ -0,0 +1,4 @@
+/* eslint-disable prettier/prettier */
+import {createContext} from 'react';
+const config = require('../../assets/config/config.json');
+export const ConfigurationContext = createContext(config);
